01be912eebbc7a2740e284370ea5c91f700b7d2d
Mathieu TAN <mathieu.tan@careerbuilder.com>
2014-12-23 14:57:26 +0100

    Starter and Empty Project

---
+++ EmptyProject/EmptyProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EmptyProject
+//
+//  Created by Mathieu Tan on 12/23/14.
+//  Copyright (c) 2014 mataejoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StarterProject/StarterProject/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  FooViewController.h
+//  StarterProject
+//
+//  Created by Mathieu Tan on 12/23/14.
+//  Copyright (c) 2014 mataejoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
